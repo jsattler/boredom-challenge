609d4ed4683d4e070c5526d574b5cce10af61d13
Joshua Sattler <34030048+jsattler@users.noreply.github.com>
2026-02-15 12:35:54 +0100

    fix: update base

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import svelte from "@astrojs/svelte";
 // https://astro.build/config
 export default defineConfig({
   site: "https://jsattler.github.io",
-  base: "boredom-trainer",
+  base: "boredom-challenge",
 
   vite: {
     plugins: [tailwindcss()],
@@ -15,3 +15,4 @@ export default defineConfig({
 
   integrations: [svelte()],
 });
+
